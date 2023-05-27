a74a86b1c1118388e7ce09a4684298d812021c17
DimaSouthTree <DITsindrin@gmail.com>
2023-05-27 15:20:44 +0300

    added function receiving_executed_operation() and receiving_date_operation()

--- utils/reading_a_json.py
+++ utils/reading_a_json.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+
 
 
 def load_operations():
@@ -9,3 +11,32 @@ def load_operations():
     with open("../operations.json", "r", encoding="utf-8") as element:
         data_operations = json.load(element)
         return data_operations
+
+
+print(load_operations())
+
+all_data_operations = load_operations()
+
+
+def receiving_executed_operation():
+    executed_list = []
+
+    for item in all_data_operations:
+        if item.get('state') == "EXECUTED":
+            executed_list.append(item)
+    return executed_list
+
+
+
+all_data_executed_operation = receiving_executed_operation()
+print(all_data_executed_operation)
+
+def receiving_date_operation():
+    date_list = []
+
+    for item in all_data_executed_operation:
+        date_list.append(item["date"])
+    return date_list
+
+
+print(receiving_date_operation())
