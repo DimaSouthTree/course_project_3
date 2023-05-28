c9cca66885df567682b4117d99ec092fe6c49df2
DimaSouthTree <DITsindrin@gmail.com>
2023-05-28 20:38:33 +0300

    added function receiving_executed_operation() and receiving_date_operation()

--- utils/reading_a_json.py
+++ utils/reading_a_json.py
@@ -1,6 +1,7 @@
 import json
-import datetime
-
+from datetime import datetime
+import datetime as DT
+from operator import itemgetter
 
 
 def load_operations():
@@ -12,8 +13,6 @@ def load_operations():
         data_operations = json.load(element)
         return data_operations
 
-
-print(load_operations())
 
 all_data_operations = load_operations()
 
@@ -27,16 +26,49 @@ def receiving_executed_operation():
     return executed_list
 
 
-
 all_data_executed_operation = receiving_executed_operation()
-print(all_data_executed_operation)
-
-def receiving_date_operation():
-    date_list = []
-
-    for item in all_data_executed_operation:
-        date_list.append(item["date"])
-    return date_list
 
 
-print(receiving_date_operation())
+def sort_operations_by_date():
+    all_data_executed_operation_sort = sorted(all_data_executed_operation, key=itemgetter("date"))
+    return all_data_executed_operation_sort[-5:]
+
+
+print(sort_operations_by_date())
+five_executed_operation = sort_operations_by_date()
+
+# Пример вывода для одной операции:
+# 14.10.2018 Перевод организации
+# Visa Platinum 7000 79** **** 6361 -> Счет **9638
+# 82771.72 руб.
+
+# def output_of_the_last_five():
+for item in five_executed_operation:
+    date = item['date']
+    dt = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f')
+    format_date = dt
+    format_date_string = format_date.strftime('%m.%d.%Y')
+
+    data_operationAmount = item['operationAmount']
+    data_operationAmount_currency = data_operationAmount['currency']
+
+    if item['description'] == 'Открытие вклада':
+        print(format_date_string, item['description'])
+        print("->", item['to'][0:4], "**" + item['to'][-4:])
+        print(data_operationAmount['amount'], data_operationAmount_currency['name'])
+        print("")
+    if item['description'] == 'Перевод со счета на счет':
+        print(format_date_string, item['description'])
+        print(item['from'][0:4], "**" + item['from'][-4:], "->", item['to'][0:4], "**" + item['to'][-4:])
+        print(data_operationAmount['amount'], data_operationAmount_currency['name'])
+        print("")
+    if item['description'] == 'Перевод организации':
+        print(format_date_string, item['description'])
+        print(item['from'][0:11] + item['from'][12:16] + item['from'][16:18] + "**", "****", item['from'][-4:], "->",
+              item['to'][0:4], "**" + item['to'][-4:])
+        print(data_operationAmount['amount'], data_operationAmount_currency['name'])
+        print("")
+
+d = {'id': 801684332, 'state': 'EXECUTED', 'date': '2019-11-05T12:04:13.781725',
+     'operationAmount': {'amount': '21344.35', 'currency': {'name': 'руб.', 'code': 'RUB'}},
+     'description': 'Открытие вклада', 'to': 'Счет 77613226829885488381'}
